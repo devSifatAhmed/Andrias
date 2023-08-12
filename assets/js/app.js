55b9bc675d50d4362b79b37b9217e3094b261cdb
programmer-sifat-ahmed <programmer.sifatahmed@gmail.com>
2023-08-12 08:44:21 -0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop()) {
+            $("header").addClass("active");
+        } else {
+            $("header").removeClass("active");
+        }
+    });
+});
